42e0f6597c3096ea9c57d054727c2a2b28e78d91
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2020-02-10 09:54:39 +0100

    Contract

--- lab05-contract/src/main/java/yncrea/lab05/contract/facade/CompanyWS.java
+++ lab05-contract/src/main/java/yncrea/lab05/contract/facade/CompanyWS.java
@@ -1,4 +1,13 @@
 package yncrea.lab05.contract.facade;
 
+import yncrea.lab05.contract.dto.CompanyDTO;
+
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService
 public interface CompanyWS {
+
+    public List<CompanyDTO> getAllCompanies();
+    public void saveCompany(CompanyDTO companyDTO);
 }
